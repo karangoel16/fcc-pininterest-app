9d25654c97d2d408a78b93d72e53f999d85300f6
karangoel16 <karangoel16@yahoo.com>
2017-05-28 13:47:34 +0530

    final changes

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,12 @@ function del(id){
 		});
 	}
 }
+
+function imgError(image) {
+	var test="/public/img/images.jpg";
+    image.onerror = "";
+    console.log(test);
+    image.src = test;
+    document.getElementById("image").value = test;
+    return true;
+}
